d63638a2b3760171430de56449e72d240a0d34ae
Yayokichi <aoiy.enjik.engineer06@gmail.com>
2026-05-14 03:01:49 +0900

    Dockerfile新規作成

--- obj/Debug/net8.0/yayokichi-csharp-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/yayokichi-csharp-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("yayokichi-csharp-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88cf24719553bef55683727f0ae414e8c233412")]
 [assembly: System.Reflection.AssemblyProductAttribute("yayokichi-csharp-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("yayokichi-csharp-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
